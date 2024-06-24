85017ae4ec5d6d6b2c2288d0498529eca1ee387f
Ucmessina <ucmessina@alumnostrazos.net>
2024-06-24 11:29:23 +0200

    +Final

--- src/js/index.js
+++ src/js/index.js
@@ -27,46 +27,39 @@ window.addEventListener('scroll', viewScroll);
 
 // - Crea un botón en tu página HTML y añade un evento de click que muestre en la consola el texto del botón al hacer click.
 const buttonElement = document.getElementById('button');
-const printClick = () => {
-  console.log('Click');
-};
+const printClick = () => console.log(buttonElement.textContent);
 buttonElement.addEventListener('click', printClick);
 
 // - Crea un h1 con el texto "Soy un título ", al ponerte con el ratón encima del h1, deberá cambiar al texto a "Quita de encima!!!" y al quitarte, deberá volver a poner "Soy un título"
-const h1Element = document.getElementById('titulo');
+const h1Element = document.getElementById('title');
 
 const originalTitleString = h1Element.textContent;
 
-const changeTitle = () => {
-  h1Element.textContent = 'Quita de encima';
-};
+const changeTitleHover = () => (h1Element.textContent = 'Quita de encima');
 
-const originalTitle = () => {
-  h1Element.textContent = originalTitleString;
-};
+const originalTitle = () => (h1Element.textContent = originalTitleString);
 
-h1Element.addEventListener('mouseover', changeTitle);
+h1Element.addEventListener('mouseover', changeTitleHover);
 h1Element.addEventListener('mouseout', originalTitle);
 // - Agrega un evento de cambio de tamaño de ventana y te muestre por consola el tamaño de la ventana.
 
-const resizeWindow = () => {
-  console.log(`Tamaño pantalla: ${window.innerWidth} x ${window.innerHeight}`);
-};
+const resizeWindow = () => console.log(`Tamaño pantalla: ${window.innerWidth} x ${window.innerHeight}`);
 
 window.addEventListener('resize', resizeWindow);
 
 // - Crea un p con el texto "esperando entrada de teclado" al pulsar cualquier tecla deberá poner qué tecla has pulsado.
 const pElement = document.getElementById('text');
+const oririnalPString = pElement.textContent;
 
-const keyPressButton = ev => {
-  pElement.textContent = `Ha pulsado ${ev.key}`;
-};
+const keyDownButton = ev => (pElement.textContent = `Has pulsado ${ev.key}`);
+const keyUpButton = () => (pElement.textContent = oririnalPString);
 
-window.addEventListener('keydown', keyPressButton);
+window.addEventListener('keydown', keyDownButton);
+window.addEventListener('keyup', keyUpButton);
 
 // - Crea un array con 5 palabras. Añade un h2 a tu HTML y ponle un id. Añade dos botones con el texto previus y next respectívamente. Haz que los botones cambien el texto del h2 con las palabras del array, cuando llegues a la última volverás a la primera si pulsas next y cuando estés en la primera podrás volver a la última dandole a previous.
 
-// const h2Element = document.getElementById('tituloH2');
+// const h2Element = document.getElementById('titleH2');
 // const previusElement = document.getElementById('previous');
 // const nextElement = document.getElementById('next');
 
@@ -74,23 +67,24 @@ window.addEventListener('keydown', keyPressButton);
 // let i = 0;
 // h2Element.textContent = setWords[i];
 
+// const changeText = () => (h2Element.textContent = setWords[i]);
+
 // const nextWord = () => {
-//   if (i >= setWords.length - 1) i = 0;
-//   else i++;
-//   h2Element.textContent = setWords[i];
+//   i >= setWords.length - 1 ? (i = 0) : i++;
+//   changeText();
 // };
 
 // const previuosWord = () => {
-//   if (i <= 0) i = setWords.length - 1;
-//   else i--;
-//   h2Element.textContent = setWords[i];
+//   i <= 0 ? setWords.length - 1 : i--;
+//   changeText();
 // };
 
 // previusElement.addEventListener('click', previuosWord);
 // nextElement.addEventListener('click', nextWord);
 
 // - Repite el mismo ejercicio pero esta vez desactiva el botón previous si estás en el primer elemento y el botón next si estás en el último
-const h2Element = document.getElementById('tituloH2');
+
+const h2Element = document.getElementById('titleH2');
 const previusElement = document.getElementById('previous');
 const nextElement = document.getElementById('next');
 
@@ -99,26 +93,22 @@ let i = 0;
 previusElement.disabled = true;
 h2Element.textContent = setWords[i];
 
+const changeText = () => (h2Element.textContent = setWords[i]);
+
 const nextWord = () => {
-  if (i < setWords.length - 1) {
-    i++;
-    previusElement.disabled = false;
-  }
-  if (i >= setWords.length - 1) {
-    nextElement.disabled = true;
-  }
-  h2Element.textContent = setWords[i];
+  i++;
+
+  if (i === setWords.length - 1) nextElement.disabled = true;
+  else previusElement.disabled = false;
+  changeText();
 };
 
 const previuosWord = () => {
-  if (i > 0) {
-    i--;
-    nextElement.disabled = false;
-  }
-  if (i <= 0) {
-    previusElement.disabled = true;
-  }
-  h2Element.textContent = setWords[i];
+  i--;
+
+  if (i === 0) previusElement.disabled = true;
+  else nextElement.disabled = false;
+  changeText();
 };
 
 previusElement.addEventListener('click', previuosWord);
@@ -136,25 +126,15 @@ const RangeInput = () => {
 rangeInputElement.addEventListener('input', RangeInput);
 // - Crea una lista de 4 checkbox con el texto que quieras y un botón, al pulsar el botón deberá decirte cuantos checkbox están marcados y cual es su texto.
 
-const checkElement = document.getElementById('checkButton');
-const checkInput = () => {
-  const checkboxesElement = document.querySelectorAll('.checkbox-item');
-  let checkedCount = 0;
-  let checkedTexts = [];
+const checkbuttonElement = document.getElementById('check-button');
+const resultElement = document.getElementById('checkbox-info');
 
-  checkboxesElement.forEach(checkbox => {
-    if (checkbox.checked) {
-      checkedCount++;
-      checkedTexts.push(checkbox.value);
-    }
-  });
+const getCheckboxInfo = () => {
+  const inputsElements = document.querySelectorAll('input:checked');
+  let resultText = `Están marcados ${inputsElements.length} inputs: `;
+  inputsElements.forEach(input => (resultText += input.value + ', '));
 
-  const resultDivElement = document.getElementById('result');
-  if (checkedCount > 0) {
-    resultDivElement.textContent = `Has marcado ${checkedCount} checkbox: ${checkedTexts.join(', ')}`;
-  } else {
-    resultDivElement.textContent = 'No has marcado ningún checkbox.';
-  }
+  resultElement.textContent = resultText;
 };
 
-checkElement.addEventListener('click', checkInput);
+checkbuttonElement.addEventListener('click', getCheckboxInfo);
